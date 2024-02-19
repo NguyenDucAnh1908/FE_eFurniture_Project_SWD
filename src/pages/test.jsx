9fdc36beb5477780b589c6cd5294b217344ad9e5
NguyenDucAnh1908 <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-02-19 14:16:56 +0700

    Update

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -15,35 +15,48 @@ const test = () => {
                                 <div className="aspect aspect--bg-grey aspect--square">
 
                                     <img className="aspect__img collection__img" src="images/collection/coll-1.jpg" alt="" /></div>
-                            </a></div>
+                            </a>
+                        </div>
                         <div className="col-lg-7 col-md-7 u-s-m-b-30">
 
                             <a className="collection" href="shop-side-version-2.html">
                                 <div className="aspect aspect--bg-grey aspect--1286-890">
 
                                     <img className="aspect__img collection__img" src="images/collection/coll-2.jpg" alt="" /></div>
-                            </a></div>
-                        <div className="col-lg-7 col-md-7 u-s-m-b-30">
+                            </a>
+                        </div>
+                        <div className="col-lg-4 col-md-5 u-s-m-b-30">
+
+                            <a className="collection" href="shop-side-version-2.html">
+                                <div className="aspect aspect--bg-grey aspect--square">
+
+                                    <img className="aspect__img collection__img" src="images/collection/coll-4.jpg" alt="" /></div>
+                            </a>
+                        </div>
+                        {/* <div className="col-lg-4 col-md-7 u-s-m-b-30">
 
                             <a className="collection" href="shop-side-version-2.html">
                                 <div className="aspect aspect--bg-grey aspect--1286-890">
 
                                     <img className="aspect__img collection__img" src="images/collection/coll-3.jpg" alt="" /></div>
-                            </a></div>
-                        <div className="col-lg-5 col-md-5 u-s-m-b-30">
+                            </a>
+                        </div> */}
+                        <div className="col-lg-4 col-md-5 u-s-m-b-30">
 
                             <a className="collection" href="shop-side-version-2.html">
                                 <div className="aspect aspect--bg-grey aspect--square">
 
                                     <img className="aspect__img collection__img" src="images/collection/coll-4.jpg" alt="" /></div>
-                            </a></div>
-                            <div className="col-lg-5 col-md-5 u-s-m-b-30">
+                            </a>
+                        </div>
+                        <div className="col-lg-4 col-md-5 u-s-m-b-30">
 
                             <a className="collection" href="shop-side-version-2.html">
                                 <div className="aspect aspect--bg-grey aspect--square">
 
                                     <img className="aspect__img collection__img" src="images/collection/coll-4.jpg" alt="" /></div>
-                            </a></div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
