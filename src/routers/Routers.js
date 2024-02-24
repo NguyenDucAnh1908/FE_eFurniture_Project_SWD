8481cc9345ef03db2da719aca59225e0a19bb6d6
NguyenDucAnh1908 <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-02-24 21:03:55 +0700

    Fix Router

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -6,7 +6,6 @@ import ProductDetail from '../pages/ProductDetail'
 import Cart from '../pages/Cart'
 import CheckOut from '../pages/CheckOut'
 import Blog from '../pages/Blog'
-import Test from '../pages/test'
 import BlogDetail from '../pages/BlogDetail'
 import MyProfile from '../pages/Account/MyProfile'
 import AddressBook from '../pages/Account/AddressBook'  
@@ -15,6 +14,9 @@ import MyOrder from '../pages/Account/MyOrder'
 import Cancellation from '../pages/Account/Cancellation'
 import PaymentOption from '../pages/Account/PaymentOption' 
 import Wishlist from '../pages/Account/Wishlist'
+
+import Test from '../pages/test'
+import IndexProductDetail from '../components/ViewProductDetail/IndexProductDetail'
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
@@ -51,6 +53,9 @@ function Routers() {
         <Route path='/wish-list' element={<Wishlist/>}/>
 
         <Route path='/test' element={<Test/>}/>
+
+        <Route exact path='/product-detail-test' element={<IndexProductDetail/>}/>
+
       </Routes>
     </BrowserRouter>
   )
