a0534377d338ed351ec632f017319bb5bfeae6ec
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-03-12 08:13:33 -0700

    fix conflict

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,101 +1,105 @@
-import React from 'react'
+import React from "react";
 // import Shop from './pages/Shop'
-import Shop from "../pages/Shop"
-import Home from "../pages/Home"
-import ProductDetail from '../pages/ProductDetail'
-import Cart from '../pages/Cart'
-import CheckOut from '../pages/CheckOut'
-import Blog from '../pages/Blog'
-import BlogDetail from '../pages/BlogDetail'
-import MyProfile from '../pages/Account/MyProfile'
-import AddressBook from '../pages/Account/AddressBook'
-import TrackOrder from '../pages/Account/TrackOrder'
-import MyOrder from '../pages/Account/MyOrder'
-import Cancellation from '../pages/Account/Cancellation'
-import PaymentOption from '../pages/Account/PaymentOption'
-import Wishlist from '../pages/Account/Wishlist'
-import AddressBookEdit from '../pages/Account/AddressBookEdit'
-import CheckOutSuccess from '../components/CheckOutSuccess/CheckOutSuccess'
-import Login from '../pages/Account/Login'
-import Register from '../pages/Account/Register'
-import NotFound from '../pages/NotFound'
-import PrivateRounter from './PrivateRount/PrivateRounter'
-import Test from '../pages/test'
-import IndexProductDetail from '../components/ViewProductDetail/IndexProductDetail'
-import ProjectBooking from '../pages/Account/ProjectBooking'
+import Shop from "../pages/Shop";
+import Home from "../pages/Home";
+import ProductDetail from "../pages/ProductDetail";
+import Cart from "../pages/Cart";
+import CheckOut from "../pages/CheckOut";
+import Blog from "../pages/Blog";
+import BlogDetail from "../pages/BlogDetail";
+import MyProfile from "../pages/Account/MyProfile";
+import AddressBook from "../pages/Account/AddressBook";
+import TrackOrder from "../pages/Account/TrackOrder";
+import MyOrder from "../pages/Account/MyOrder";
+import Cancellation from "../pages/Account/Cancellation";
+import PaymentOption from "../pages/Account/PaymentOption";
+import Wishlist from "../pages/Account/Wishlist";
+import AddressBookEdit from "../pages/Account/AddressBookEdit";
+import CheckOutSuccess from "../components/CheckOutSuccess/CheckOutSuccess";
+import Login from "../pages/Account/Login";
+import Register from "../pages/Account/Register";
+import NotFound from "../pages/NotFound";
+import PrivateRounter from "./PrivateRount/PrivateRounter";
+import Test from "../pages/test";
+import IndexProductDetail from "../components/ViewProductDetail/IndexProductDetail";
+import ProjectBooking from "../pages/Account/ProjectBooking";
 
-
-import EditProfile from '../pages/Account/EditProfile'
-import ManagerOrderDetail from '../pages/Account/ManagerOrderDetail'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import EditProfile from "../pages/Account/EditProfile";
+import ManagerOrderDetail from "../pages/Account/ManagerOrderDetail";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function Routers() {
-
   return (
     <>
       {/* <BrowserRouter> */}
-        <Routes>
-          <Route path='/shop' element={<Shop />} />
+      <Routes>
+        <Route path="/shop" element={<Shop />} />
 
-          <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
 
-          <Route path='/login' element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
-          <Route path='/register' element={<Register />} />
+        <Route path="/register" element={<Register />} />
 
-          <Route path='/product-detail/:id' element={<ProductDetail />} />
+        <Route path="/product-detail/:id" element={<ProductDetail />} />
 
-          <Route path='/cart' element={<Cart />} />
+        <Route path="/cart" element={<Cart />} />
 
-          {/* <Route path='/check-out' element={<CheckOut/>}/> */}
+        {/* <Route path='/check-out' element={<CheckOut/>}/> */}
 
-          <Route path='/check-out/thank-you' element={<CheckOutSuccess />} />
+        <Route path="/check-out/thank-you" element={<CheckOutSuccess />} />
 
-          <Route path='/blog' element={<Blog />} />
+        <Route path="/blog" element={<Blog />} />
 
-          <Route path='/blog-detail/:id' element={<BlogDetail />} />
+        <Route path="/blog-detail/:id" element={<BlogDetail />} />
 
-          <Route path='/my-profile' element={<MyProfile />} />
+        <Route path="/my-profile" element={<MyProfile />} />
 
-          <Route path='/address-book' element={<AddressBook />} />
+        <Route path="/address-book" element={<AddressBook />} />
 
-          <Route path='/my-order' element={<MyOrder />} />
+        <Route path="/my-order" element={<MyOrder />} />
 
-          <Route path='/track-order' element={<TrackOrder />} />
-        <Route path='/edit-profile' element={<EditProfile/>}/>
-        <Route path='/manager-orderdetail' element={<ManagerOrderDetail/>}/>
-        <Route path='/track-order' element={<TrackOrder/>}/>
+        <Route path="/track-order" element={<TrackOrder />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/manager-orderdetail" element={<ManagerOrderDetail />} />
+        <Route path="/track-order" element={<TrackOrder />} />
 
-          <Route path='/cancellation' element={<Cancellation />} />
+        <Route path="/cancellation" element={<Cancellation />} />
 
-          <Route path='/payment-option' element={<PaymentOption />} />
+        <Route path="/payment-option" element={<PaymentOption />} />
 
-          <Route path='/wish-list' element={<Wishlist />} />
+        <Route path="/wish-list" element={<Wishlist />} />
 
-          <Route path='/edit-address' element={<AddressBookEdit />} />
-          <Route path='/view-list-projectbooking/:bookingId' element={<ProjectBooking />} />
-          <Route path='*' element={<NotFound />} />
+        <Route path="/edit-address" element={<AddressBookEdit />} />
+        <Route
+          path="/view-list-projectbooking/:bookingId"
+          element={<ProjectBooking />}
+        />
+        <Route path="*" element={<NotFound />} />
 
-          {/* {user && user.isLoading ?
+        {/* {user && user.isLoading ?
           
         } */}
-          <Route
-            path="/check-out" element={
-              <PrivateRounter >
-                <CheckOut />
-              </PrivateRounter>
-              
-            }
-          />
+        <Route
+          path="/check-out"
+          element={
+            <PrivateRounter>
+              <CheckOut />
+            </PrivateRounter>
+          }
+        />
 
-          <Route path='/test' element={<Test />} />
+        <Route path="/test" element={<Test />} />
 
-          <Route exact path='/product-detail-test' element={<IndexProductDetail />} />
-
-        </Routes>
+        <Route
+          exact
+          path="/product-detail-test"
+          element={<IndexProductDetail />}
+        />
+      </Routes>
       {/* </BrowserRouter> */}
     </>
-  )
+  );
 }
 
-export default Routers
+export default Routers;
